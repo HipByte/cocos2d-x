6f14d3525d5a8939f20b24bd17d2eac775922ed3
Watson <watson1978@gmail.com>
2016-03-25 09:26:56 +0900

    Revert " Android NDK r11 does not provide cpu-features.h for Intel CPU platform"
    I could find it in /ndk/sources/android/cpufeatures/cpu-features.h
    
    This reverts commit a54ca352098661648a0c10b3ea4c3eeb47d1d77d.

--- cocos/math/MathUtil.cpp
+++ cocos/math/MathUtil.cpp
@@ -21,8 +21,7 @@ This file was modified to fit the cocos2d-x project
 #include "MathUtil.h"
 #include "base/ccMacros.h"
 
-// Android NDK r11 does not provide cpu-features.h for Intel CPU platform
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID && (defined(__arm__) || defined(__arm64__) || defined (__aarch64__)))
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
 #include <cpu-features.h>
 #endif
 
